c353de3a51580cf1f126d98cda759ceafa01862e
Ggirassol <gi-12@hotmail.com>
2025-01-13 18:31:47 +0000

    endpoint for login renamed and server set on file apart

--- app.js
+++ app.js
@@ -4,15 +4,8 @@ app.use(express.json());
 const { getIntakeByDate, addUser } = require("./controllers.js");
 const { handleCustomErrors } = require("./handleCustomErrors.js");
 
-if (process.env.NODE_ENV !== "test") {
-    const PORT = process.env.PORT || 3000;
-    app.listen(PORT, () => {
-      console.log(`The server is active on port ${PORT}`);
-    });
-}
-
 app.get("/api/:userId/:date", getIntakeByDate);
-app.post("/api/users", addUser)
+app.post("/api/register", addUser)
 app.use(handleCustomErrors)
 
 module.exports = app;
