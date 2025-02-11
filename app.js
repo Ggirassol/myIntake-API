0235267f070981d681ea2748a499207b2765d266
Ggirassol <gi-12@hotmail.com>
2025-02-11 21:39:58 +0000

    add cors

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+app.use(cors());
 app.use(express.json());
 const { getIntakeByDate, addUser, loginUser, createNewToken, addIntake, logoutUser, getEndpointsDescription } = require("./controllers.js");
 const { handleCustomErrors } = require("./handleCustomErrors.js");
